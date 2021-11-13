da98b09609c822324f3dbd7d46a63c51e2c2fb3f
Dong-yeob <dong333j@gmail.com>
2021-11-13 15:44:01 +0900

    도서관 자료구조 - 카테고리 · 출판사 · 도서 리스트 출력 구현

--- Example/Example/ExDictionary.cs
+++ Example/Example/ExDictionary.cs
@@ -10,21 +10,64 @@ namespace Example
     {
         private Dictionary<string, Dictionary<string, List<string>>> libraryData;
 
+        // 도서관 데이터 추가
         public Library()
         {
             libraryData = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            Dictionary<string, List<string>> classicalLiteraturePublisher = new Dictionary<string, List<string>>();
+            Dictionary<string, List<string>> lightNovelPublisher = new Dictionary<string, List<string>>();
+
+            libraryData.Add("Classical", classicalLiteraturePublisher);
+            libraryData.Add("LightNovel", lightNovelPublisher);
+
+            // 출판사 & 도서 추가
+
+            List<string> minumsa = new List<string>();
+            minumsa.Add("인간 실격");
+            minumsa.Add("이방인");
+            minumsa.Add("전쟁과 평화");
+            classicalLiteraturePublisher.Add("Minumsa", minumsa);
+
+            List<string> theStroy = new List<string>();
+            theStroy.Add("데미안");
+            theStroy.Add("어린왕자");
+            theStroy.Add("월든");
+            classicalLiteraturePublisher.Add("TheStory", theStroy);
+
+            List<string> daewonCI = new List<string>();
+            daewonCI.Add("너의 이름은");
+            daewonCI.Add("날씨의 아이");
+            daewonCI.Add("스즈미야 하루히의 직관");
+            lightNovelPublisher.Add("DaewonCI", daewonCI);
+
+            List<string> haksan = new List<string>();
+            haksan.Add("무직전생");
+            haksan.Add("늑대와 향신료");
+            haksan.Add("알바 뛰는 마왕님");
+            lightNovelPublisher.Add("Haksan", haksan);
         }
 
-        public void Menu()
+        public void Search()
         {
+            PrintCategory();
 
+            Console.WriteLine("\n도서 카테고리를 입력하세요.");
+            string category = Console.ReadLine();
 
+            PrintPublisher(category);
+
+            Console.WriteLine("\n도서 리스트를 볼 출판사를 입력하세요.");
+            string pub = Console.ReadLine();
+
+            PrintBookList(category, pub);
         }
-
         
         public void PrintCategory()
         {
-            // 도서관의 카테고리 모두 출/
+            // 도서관의 카테고리 모두 출력
+
+            Console.WriteLine("도서 카테고리");
 
             foreach (string item in libraryData.Keys)
             {
@@ -32,61 +75,43 @@ namespace Example
             }
         }
 
-        public void PrintPublisher()
+        public void PrintPublisher(string category)
         {
             // 도서관 카테고리의 출판사 모두 출력
+
+            if (libraryData.ContainsKey(category))
+            {
+                Console.WriteLine("\n{0} 출판사", category);
+
+                foreach (string item in libraryData[category].Keys)
+                {
+                    Console.WriteLine("- {0}", item);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("- 해당하는 카테고리가 없습니다.");
+            }
         }
 
-        public void PrintBookList()
+        public void PrintBookList(string category, string publisher)
         {
             // 도서관 카테고리 - 출판사의 책 모두 출력
-            
-        }
 
-        public void GetCategoryPublisher(string key)
-        {
-            if (libraryData.ContainsKey(key))
+            if (libraryData[category].ContainsKey(publisher))
             {
+                Console.WriteLine("\n{0} 의 도서", publisher);
 
-            }
-            else
-            {
-                Console.WriteLine("해당하는 도서 카테고리가 없습니다.");
-            }
-        }
-
-        public void AddBook(string bookName, string publisherName, string category)
-        {
-            bool isBookExist = false;
-            bool isPublisherExist = false;
-            bool isCategoryExist = false;
-
-
-            if (libraryData.ContainsKey(category))
-            {
-                isCategoryExist = true;
-
-                if (libraryData.ContainsKey(publisherName))
+                foreach (string item in libraryData[category][publisher])
                 {
-                    isPublisherExist = true;
-
-
-                    if (libraryData.ContainsKey(bookName))
-                    {
-                        isBookExist = true;
-
-                    }
+                    Console.WriteLine("- {0}", item);
                 }
-            }
-
-
-            if (libraryData.ContainsKey(category))
-            {
 
             }
             else
             {
-                Dictionary<string, List<string>> categoryPublisher = new Dictionary<string, List<string>>();
+                Console.WriteLine("- 해당하는 출판사가 없습니다.");
             }
         }
     }
@@ -95,21 +120,9 @@ namespace Example
     {
         static void Main()
         {
-            Dictionary<string, Dictionary<string, List<string>>> bookCategory = new Dictionary<string, Dictionary<string, List<string>>>();
+            Library myLibrary = new Library();
 
-            Dictionary<string, List<string>> classicalLiteraturePublisher = new Dictionary<string, List<string>>();
-            Dictionary<string, List<string>> domesticNovelPublisher = new Dictionary<string, List<string>>();
-            Dictionary<string, List<string>> foreignLiteraturePublisher = new Dictionary<string, List<string>>();
-            Dictionary<string, List<string>> lightNovelPublisher = new Dictionary<string, List<string>>();
-
-            bookCategory.Add("Classical", classicalLiteraturePublisher);
-            bookCategory.Add("Domestic", domesticNovelPublisher);
-            bookCategory.Add("Foreign", foreignLiteraturePublisher);
-            bookCategory.Add("LightNovel", lightNovelPublisher);
-
-            List<string> haksan = new List<string>();
-
-
+            myLibrary.Search();
         }
     }
 }
