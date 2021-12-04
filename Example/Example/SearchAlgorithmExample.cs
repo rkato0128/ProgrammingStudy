cf5df97c16f28738252b526e9292b2867854ffcc
Dong-yeob <rkato0128@naver.com>
2021-12-04 18:15:52 +0900

    이진탐색 알고리즘 학습

---
+++ Example/Example/SearchAlgorithmExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class SearchAlgorithmExample
+    {
+        static int[] array = new int[] {1, 6, 8, 13, 20};
+        static int time = 0;
+
+        static void Main()
+        {
+            Console.WriteLine(BinarySearch(array, 0, array.Length - 1, 20));
+            Console.WriteLine(time);
+
+        }
+
+        static int BinarySearch(int[] array, int start, int end, int target)
+        {
+            time++;
+
+            int half = (end - start) / 2;
+
+            if (array[start + half] == target)
+            {
+                return start + half;
+            }
+            else if (array[start + half] > target)
+            {
+                return BinarySearch(array, start, start + half - 1, target);
+            }
+            else if (array[start + half] < target)
+            {
+                return BinarySearch(array, start + half + 1, end, target);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Example/Example/ThreadExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class ThreadExample
+    {
+        static int temp = 0;
+
+        static void Main()
+        {
+            Thread t1 = new Thread(AddNum);
+            Thread t2 = new Thread(AddNum);
+            Thread t3 = new Thread(AddNum);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+
+        static void AddNum()
+        {
+            temp++;
+            Console.WriteLine(temp);
+        }
+    }
+}
